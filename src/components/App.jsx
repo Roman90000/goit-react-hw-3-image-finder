8965c0aa5277bd775480f7bd32d1103963345e00
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-09-05 19:54:30 +0300

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,39 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { responseAxios } from 'api';
 import toast, { Toaster } from 'react-hot-toast';
+import { Button } from './Button/Button';
+import { CustomModal } from './Modal/Modal';
 
 export class App extends Component {
   state = {
     input: '',
     images: [],
     page: 1,
+    modalIsOpen: false,
+    webformatURL: '',
+    tags: '',
   };
+
+  // componentDidMount() {
+  //   const savedLocal = localStorage.getItem('state');
+  //   const localState = JSON.parse(savedLocal);
+  //   const { input, images, page } = localState;
+  //   if (savedLocal !== null) {
+  //     this.setState({
+  //       input: input,
+  //       images: images,
+  //       page: page,
+  //     });
+  //   }
+  // }
 
   componentDidUpdate(prevProps, prevState) {
     const { input, page } = this.state;
+
+    // if (prevState.state !== this.state) {
+    //   localStorage.setItem('state', JSON.stringify(this.state));
+    // }
+
     if (prevState.input !== input || prevState.page !== page) {
       responseAxios(input, page)
         .then(data => {
@@ -39,11 +62,41 @@ export class App extends Component {
     });
   };
 
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  openModal = ({ webformatURL, tags }) => {
+    this.setState(prevState => ({
+      modalIsOpen: !prevState.modalIsOpen,
+      webformatURL,
+      tags,
+    }));
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalIsOpen: false,
+      webformatURL: '',
+    });
+  };
   render() {
+    const { images, webformatURL, tags, modalIsOpen } = this.state;
     return (
       <div>
         <Searchbar search={this.searchInput} />
-        {this.state.images.length > 0 && <ImageGallery />}
+        {this.state.images.length > 0 && (
+          <ImageGallery openModal={this.openModal} images={images} />
+        )}
+        {this.state.images.length > 0 && <Button onLoadMore={this.loadMore} />}
+        <CustomModal
+          closeModal={this.closeModal}
+          modalIsOpen={modalIsOpen}
+          image={webformatURL}
+          tags={tags}
+        />
         <Toaster position="top-right" />
       </div>
     );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,11 @@
-export const ImageGallery = () => {
-  return <ul></ul>;
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = ({ images, openModal }) => {
+  return (
+    <ul>
+      {images.map(image => (
+        <ImageGalleryItem openModal={openModal} key={image.id} image={image} />
+      ))}
+    </ul>
+  );
 };
